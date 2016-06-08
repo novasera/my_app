e7d6d6e78ef756a9124f6dbae701130e16eae164
novasera <novasera@naver.com>
2016-06-08 17:57:50 +0900

    20160608_commit_3

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ var app = express();
 var mongoose = require('mongoose');
 
 // connect Mongo DB
-mongoose.connect("mongodb://dev:devdev@ds011024.mlab.com:11024/novasera");
+
 var db = mongoose.connection;
 
 db.once("open", function() {
@@ -15,30 +15,81 @@ db.on("error", function(e) {
   console.log("DB ERROR : ", e);
 });
 
+var dataSchema = mongoose.Schema({
+  name:String,
+  count:Number
+});
+
+var Data = mongoose.model('data', dataSchema);
+
+Data.findOne( {name:"myData"}, function(e, data) {
+
+    if (e) return console.log("Data ERROR : ", e);
+
+    if (!data)
+    {
+      Data.create( {name:"myData", count:0}, function(e, data) {
+        if (e) return console.log("Data create ERROR : ", e);
+
+        console.log("Counter initialized : ", data);
+      });
+    }
+});
+
 app.set("view engine", 'ejs');
 app.use(express.static(path.join(__dirname, 'public')));
 
-var data = {count:0};
-
 app.get('/', function (req, res) {
-  data.count++;
-  res.render('my_first_ejs', data);
+  Data.findOne( {name:"myData"}, function(e, data) {
+    if (e) return console.log("Data ERROR", e);
+    data.count++;
+
+    data.save(function (e) {
+      if (e) return console.log("Data Save ERROR", e);
+      res.render("my_first_ejs", data);
+    });
+  });
 });
 
 app.get('/reset', function (req, res) {
-  data.count = 0;
-  res.render('my_first_ejs', data);
+  setCounter(res, 0);
 });
 
 app.get('/set/count', function (req, res) {
-  if (req.query.count) data.count = req.query.count;
-  res.render('my_first_ejs', data);
+  if (req.query.count) setCounter(res, req.query.count);
+  else getCounter(res);
 });
 
 app.get('/set/:num', function (req, res) {
-  data.count = req.params.num;
-  res.render('my_first_ejs', data);
+  if (req.params.num) setCounter(res, req.params.num);
+  else getCounter(res);
 });
+
+function setCounter(res, num)
+{
+  console.log("setCounter");
+
+  Data.findOne( {name:"myData"}, function(e, data) {
+    if (e) return console.log("Data ERROR", e);
+    data.count = num;
+
+    data.save(function (e) {
+      if (e) return console.log("Data Save ERROR", e);
+      res.render("my_first_ejs", data);
+    });
+  });
+}
+
+function getCounter(res)
+{
+  console.log("getCounter");
+
+  Data.findOne( {name:"myData"}, function(e, data) {
+    if (e) return console.log("Data ERROR", e);
+
+    res.render("my_first_ejs", data);
+  });
+}
 
 app.listen(3000, function() {
   console.log('Server On!');
